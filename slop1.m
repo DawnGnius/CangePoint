e694a860b07e984f06ad0477bb2766f0208c4dec
LENOVO-DAWN\dawng <184050473@qq.com>
2018-03-14 12:03:51 +0800

    Generate the slope change point data

--- slop1.m
+++ slop1.m
@@ -1,4 +1,11 @@
 %% 模拟数据
-
-
+a = 0; b = 0; c = 1; tau = 0.5;
+x = 0.01 : 0.01 : 1;% 不能从 0 开始, 向量化有些小问题.
+xx = [x;x];
+xx(1,xx(1,:)>tau) = 0;xx(2,xx(2,:)<=tau) = 0;
+slope = [a, c];
+tmp = xx;tmp(tmp > 0) = 1;
+intercept = [b, a * tau + b - c * tau] * tmp;
+y = slope * xx + intercept;
+plot(x, y)
 %% 估计
