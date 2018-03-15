7d3ce8f3b9d0f68bbec1606cd018f77f31d26c24
Gnius <dawn.gnius@outlook.com>
2018-03-15 23:46:16 +0800

    Change into Inter SSD, and Convert into English with encoding UTF-8. Functionally unchanged.

--- slop1.m
+++ slop1.m
@@ -1,7 +1,7 @@
 format compact
-%% 模拟数据
+%% Simulation
 a = 0; b = 0; c = 3; n = 500; L = 50; tau = 0; sigma = 0.03; repeat =1000; 
-x = 1/n : 1/n : 1;% 不能从 0 开始, 向量化有些小问题.
+x = 1/n : 1/n : 1;% Start from 0 could lead to a problem in the following vectorization process.
 xx = [x;x];
 xx(1,xx(1,:)>tau) = 0; xx(2,xx(2,:)<=tau) = 0;
 slope = [a, c];
@@ -12,12 +12,12 @@ for num = 1: repeat
     epsilon = sigma * randn(1,n);
     y = slope * xx + intercept + epsilon;
     % plot(x, y)
-    %% 估计
+    %% Estimation
     tmp = (floor(n/2)-L) * n^2;
     A = [1; 1/tmp];
     for ii= 1+1 : L
         A = [A, [1; ii/tmp]];
-    end%数字太小了, 会不会有很大的影响
+    end % Caution: Numeric error
     for ii = 1 : L
         tmp = 0;
         for jj = 1 : floor(n/2) - L
